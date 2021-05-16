07436121010ca1abe1e0a7f2185497e55bef9ca1
Thomas Wade <gigatech55@gmail.com>
2021-05-16 14:57:35 +0930

    Add a bunch of uncommitted code
    This must've been collecting dust for the past 2 years now, crikey.

--- avr/main.c
+++ avr/main.c
@@ -11,6 +11,7 @@
 
 #include <avr/io.h>
 #include <util/delay.h>
+#include <string.h>
 
 #define BAUDRATE 9600
 
@@ -37,7 +38,7 @@ int main(void) {
     init();
 
     while (1) {
-        wait_for_str("GNU GRUB");
+        if (!compareString(serial_buffer, "GNU GRUB")) continue;
 
         _delay_ms(2000);
 
@@ -56,4 +57,62 @@ int main(void) {
     }
 
     return 0;
+}
+
+int compareString(char* str, char* template)
+{
+    int matches = 0;
+    unsigned short str_start = str;
+    unsigned short template_start = template;
+
+    while (*str && *template)
+    {
+        matches += *str == *template ? 1 : 0;
+    }
+
+    switch ()
+}
+
+unsigned char ReverseByte (unsigned char x) {
+    x = ((x >> 1) & 0x55) | ((x << 1) & 0xaa);
+    x = ((x >> 2) & 0x33) | ((x << 2) & 0xcc);
+    x = ((x >> 4) & 0x0f) | ((x << 4) & 0xf0);
+    return x;
+}
+
+// Initialise USI for UART reception.
+void InitialiseUSI (void) {
+    DDRB &= ~(1<<DataIn);           // Define DI as input
+    USICR = 0;                      // Disable USI.
+    GIFR = 1<<PCIF;                 // Clear pin change interrupt flag.
+    GIMSK |= 1<<PCIE;               // Enable pin change interrupts
+    PCMSK |= 1<<PCINT0;             // Enable pin change on pin 0
+}
+
+// Pin change interrupt detects start of UART reception.
+ISR (PCINT0_vect) {
+        if (PINB & 1<<DataIn) return;   // Ignore if DI is high
+        GIMSK &= ~(1<<PCIE);            // Disable pin change interrupts
+        TCCR0A = 2<<WGM00;              // Timer in CTC mode
+        TCCR0B = 2<<CS00;               // Set prescaler to /8
+        OCR0A = 103;                    // Shift every (103+1)*8 cycles
+        TCNT0 = 206;                    // Start counting from (256-52+2)
+        // Enable USI OVF interrupt, and select Timer0 compare match as USI Clock source:
+        USICR = 1<<USIOIE | 0<<USIWM0 | 1<<USICS0;
+        USISR = 1<<USIOIF | 8;          // Clear USI OVF flag, and set counter
+}
+
+// USI overflow interrupt indicates we've received a byte
+ISR (USI_OVF_vect) {
+        USICR = 0;                      // Disable USI
+        int temp = USIDR;
+        Display(ReverseByte(temp));
+        GIFR = 1<<PCIF;                 // Clear pin change interrupt flag.
+        GIMSK |= 1<<PCIE;               // Enable pin change interrupts again
+}
+
+// Reads a single char from the USI register
+char readChar()
+{
+
 }
